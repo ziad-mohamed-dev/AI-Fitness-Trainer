ae2fc5cd3c46ec01f27b0b0fd317c798aba9a13c
ziad-mohamed-dev <ziadonwork@gmail.com>
2025-05-09 21:05:55 +0300

    fixing not loaded images in users gallary

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/api/portraits/men/**",
-      },
       {
         protocol: "https",
         hostname: "img.clerk.com",
